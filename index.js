2bc7bce729f5a7f926a185800eb6063222c7b3da
Fahad Ali Babar <rajafahadali931@gmail.com>
2025-08-09 18:31:35 +0500

    finall

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ const express=require("express")
 require('dotenv').config()
 const app=express();
 const port=process.env.port||4000;
+const githubdata={
+    "login":"fahad",
+    "id":"02",
+    "email":"fahad931@gmail.com"
+}
 app.get("/",(req,res)=>{
     res.send("fahad ali babar")
 
 })
 app.get("/login",(req,res)=>{
     res.send("fahad@gmail.com")
+
+})
+app.get("/github",(req,res)=>{
+    res.json(githubdata)
 
 })
 app.listen(port,()=>{
